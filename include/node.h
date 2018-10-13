87c24cd30038f55298dc1755f80268f5a970a393
kaitou_ryaku <34281612+kaitou-ryaku@users.noreply.github.com>
2018-10-13 16:24:31 +0900

    node.cのユニットテストの呼び出し関数のバグを修正

--- include/node.h
+++ include/node.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void (*__static_node_func__()) (void);
+void (*__static_node_func__(const char *func_name)) (void);
 void regex_to_all_node( const char* regex_str, NODE* node, const int node_list_size);
 
 #endif
